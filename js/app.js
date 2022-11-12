77ea1bcb6fd024693a5f695937ec661c17f4b9c0
Cata <catalina.zapata@usach.cl>
2022-11-12 13:37:27 -0300

    V0R4: Mensajes customizados

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
 import { valida } from "./validaciones.js";//Muy bien, ya tenemos entonces aquí nuestra función de valida. Ahora lo que nosotros necesitamos es agregar el addEventListener que va a mandar a llamar esta función cada vez que el input sufra el cambio o tenga el cambio de blur, cada vez que salga el usuario de ese input.
 
-const inputs = documento.querySelectorAll("input");//selecciona todos los inputs
+const inputs = document.querySelectorAll("input");//selecciona todos los inputs
 
-inputs.forEach(input => {//le agrega a todos los inputs los addeventlistener del blur
-    inputs.addEventListener("blur", (input) => {
-    valida(input.target)//cuando salga  amndara a llamar esta funcion
-    })    
+inputs.forEach ((input) => {//le agrega a todos los inputs los addeventlistener del blur
+    input.addEventListener("blur", (input) => {
+    valida(input.target);//cuando salga  amndara a llamar esta funcion
+    });
 });
+
 
 // Después, inputs. Yo cuando digo document.querySelectorAll lo que me va a regresar es un arreglo. Y como recuerdas, los arreglos los podemos iterar, lo cual voy a hacer aquí. Lo que vamos a recibir aquí es nuestro input, y lo que voy a hacer entonces es para cada uno de los inputs vamos a obtener input, lo que vamos a tener entonces es a nuestro input.addEventListener le vamos a decir cuál es el tipo de evento que queremos escuchar.
--- js/validaciones.js
+++ js/validaciones.js
@@ -3,13 +3,59 @@ export function valida(input){//reemplaza a llamar al input con el id
      if (validadores[TipoDeInput]){//ayuda a verificar si cada uno de los input corresponde a los validadores
         validadores[TipoDeInput](input);
      }
+    
+    if(input.validity.valid){//ayuda a que se coloree la casilla cuando no se completa bien
+        input.parentElement.classList.remove("input-container--invalid");//si es true la quita
+        input.parentElement.querySelector(".input-message-error").innerHTML = "";
+    } else{
+        input.parentElement.classList.add("input-container--invalid");//si es false la grega
+        input.parentElement.querySelector(".input-message-error").innerHTML =  mostrarMensajeDeError(tipoDeInput, input);
+    }
 }
 //Esta función valida vive aquí en validaciones, va a recibir el input, va a verificar cuál es el tipo de input a través de dataset.tipo ..... y a partir de ahí lo que vamos a tener que ir armando, pues bueno, es aquí todo un objeto con los diferentes tipos de input que vamos a tener.
+
+const tipoDeErrores = [
+    "valueMissing",
+    "typeMismatch",
+    "patternMismatch",
+    "customError",
+  ];
+
+const mensajesDeError = {
+    nombre: {
+      valueMissing: "El campo nombre no puede estar vacío",
+    },
+    email: {
+      valueMissing: "El campo correo no puede estar vacío",
+      typeMismatch: "El correo no es válido",
+    },
+    password: {
+      valueMissing: "El campo contraseña no puede estar vacío",
+      patternMismatch:"Al menos 6 caracteres, máximo 12, debe contener una letra minúscula, una letra mayúscula, un número y no puede contener caracteres especiales.",
+    },
+    nacimiento: {
+      valueMissing: "Este campo no puede estar vacío",
+      customError: "Debes tener al menos 18 años de edad",
+    },
+  };
 
 const validadores = {//nombre del tipo con la llave dentro del sujeto  
     nacimiento: input => validarNacimiento(input),
 };
 //y a partir de ahí lo que vamos a tener que ir armando, pues bueno, es aquí todo un objeto con los diferentes tipos de input que vamos a tener.
+
+function mostrarMensajeDeError(tipoDeInput, input) {
+    let mensaje = "";
+    tipoDeErrores.forEach((error) => {
+      if (input.validity[error]) {
+        console.log(tipoDeInput, error);
+        console.log(input.validity[error]);
+         console.log(mensajesDeError[tipoDeInput][error]);
+        mensaje = mensajesDeError[tipoDeInput][error];
+      }
+    });
+    return mensaje;
+  }
 
 function validarNacimiento(input) {//obtiene el valor de la fecha nacimiento y valida o no la mayoria de edad
     const fechaCliente = new Date(input.value);//El operador new permite a los desarrolladores crear una instancia de un tipo de objeto definido por el usuario o de uno de los tipos de objeto integrados que tiene un función constructora.
